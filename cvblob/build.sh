b95470db1a9a7a96a2db108b75824f373a14a5bc
Manuel Francisco Naranjo <naranjo.manuel@gmail.com>
2012-10-17 00:05:18 -0300

    use pkg-config instead of hardcoded paths

--- cvblob/build.sh
+++ cvblob/build.sh
@@ -1,2 +1,5 @@
-g++ -fPIC -I/usr/include/python2.6 -I/usr/include -I/usr/local/include -c _cvblob.C
-g++ -shared -Wl _cvblob.o -L/usr/lib -L/usr/local/lib -lopencv_core -lcvblob -lboost_python -L/usr/lib/python2.6/config -lpython2.6 -o _cvblob.so
+INC=$(pkg-config --cflags python2 opencv)
+PYCFLAGS=$(pkg-config --libs python2)
+g++ -fPIC ${INC} -c _cvblob.C
+g++ -shared -Wl ${PYCFLAGS} _cvblob.o \
+    -lopencv_core -lcvblob -lboost_python -o _cvblob.so
